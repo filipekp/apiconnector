9894e67ac4f2431851d77830fa7d8bdcaba1a3f3
Pavel Filípek <filipek@proclient.cz>
2019-03-08 15:39:15 +0100

    -oprava

--- src/Connector.php
+++ src/Connector.php
@@ -3,7 +3,7 @@
   namespace apiconnector;
   
   use PF\helpers\MyArray;
-
+  
   /**
    * Třída Connector slouží pro komunikaci s API e-shopů.
    *
@@ -37,11 +37,11 @@
     private static $cookieIndex = '';
     
     private $tmpDir = NULL;
-  
+    
     private $lastTokenFile = '';
     private $cookieFile = '';
     private $logged = FALSE;
-  
+    
     /**
      * Connector constructor.
      *
@@ -52,14 +52,14 @@
     public function __construct($apiUrl, $apiKey, $tmpDir = NULL) {
       $this->apiUrl = $apiUrl;
       $this->apiKey = $apiKey;
-  
+      
       self::$cookieIndex   = md5(json_encode([$apiUrl, $apiKey]));
       $this->tmpDir        = rtrim(((is_null($tmpDir)) ? sys_get_temp_dir() : $tmpDir), '/') . '/';
       $this->lastTokenFile = $this->tmpDir . 'lasttoken_' . self::$cookieIndex;
       $this->cookieFile    = $this->tmpDir . 'cookies_' . self::$cookieIndex;
       $this->token         = @file_get_contents($this->lastTokenFile);
     }
-  
+    
     /**
      * Zajistí login do API.
      *
@@ -69,18 +69,18 @@
      */
     private function login($countTry) {
       $success  = FALSE;
-      $response = $this->callApi('login', ['key' => $this->apiKey], TRUE, $countTry);
-  
-      if ($response && array_key_exists('token', $response) && $response['token']) {
-        $this->state = (bool)$response['token'];
+      $response = (array)$this->callApi('login', ['key' => $this->apiKey], TRUE, $countTry);
+      
+      if ($response &&  ($status = MyArray::init($response)->item('status',  MyArray::init($response)->item('success', FALSE)))) {
+        $this->state = (bool)$status;
         $this->token = $response['token'];
         file_put_contents($this->lastTokenFile, $this->token);
         $success = TRUE;
       }
-  
+      
       return $success;
     }
-  
+    
     /**
      * Zajištujě zavolání API metody.
      *
@@ -94,49 +94,49 @@
     public function callApi ($url, $paramsArray = [], $responseAsArray = TRUE, $countTry = 3) {
       /** @var string $link */
       $link = $this->apiUrl . $url . (($this->token) ? '&token=' . $this->token : '');
-  
-      $ch = curl_init();
-        curl_setopt($ch, CURLOPT_HTTPHEADER, [
-          'Expect:',
-          'Accept-Encoding: gzip, deflate'
-        ]);
-        curl_setopt($ch, CURLOPT_ENCODING, "gzip");
-        curl_setopt($ch, CURLOPT_URL, $link);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-        curl_setopt($ch, CURLOPT_TIMEOUT, $this->requestTimeout);
-        curl_setopt($ch, CURLOPT_MAXREDIRS, 10);
-        curl_setopt($ch, CURLOPT_AUTOREFERER, TRUE);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-        curl_setopt($ch, CURLOPT_USERAGENT, "ApiConnector-proclient-ver." . self::VERSION);
-        curl_setopt($ch, CURLOPT_COOKIEJAR, $this->cookieFile);
-        curl_setopt($ch, CURLOPT_COOKIEFILE, $this->cookieFile);
-        curl_setopt($ch, CURLOPT_POST, count($paramsArray));
-        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($paramsArray));
-        
-        //execute post
-        $result             = curl_exec($ch);
-        $this->lastResponse = ['content' => $result, 'headers' => curl_getinfo($ch)];
-  
-        //close connection
-        curl_close($ch);
-        $decodedResult = json_decode($result, $responseAsArray);
-        if ($decodedResult) {
-          $decodedResultArr = MyArray::init($decodedResult);
-          if ($countTry > 0 && $decodedResultArr->item('status', $decodedResultArr->item('success', FALSE)) == FALSE && !$this->logged) {
-            $countTry--;
-            if ($this->login($countTry)) {
-              $this->logged = TRUE;
-              $decodedResult = $this->callApi($url, $paramsArray, $responseAsArray);
-            }
-          }
       
-          $result = $decodedResult;
+      $ch = curl_init();
+      curl_setopt($ch, CURLOPT_HTTPHEADER, [
+        'Expect:',
+        'Accept-Encoding: gzip, deflate'
+      ]);
+      curl_setopt($ch, CURLOPT_ENCODING, "gzip");
+      curl_setopt($ch, CURLOPT_URL, $link);
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+      curl_setopt($ch, CURLOPT_TIMEOUT, $this->requestTimeout);
+      curl_setopt($ch, CURLOPT_MAXREDIRS, 10);
+      curl_setopt($ch, CURLOPT_AUTOREFERER, TRUE);
+      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+      curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+      curl_setopt($ch, CURLOPT_USERAGENT, "ApiConnector-proclient-ver." . self::VERSION);
+      curl_setopt($ch, CURLOPT_COOKIEJAR, $this->cookieFile);
+      curl_setopt($ch, CURLOPT_COOKIEFILE, $this->cookieFile);
+      curl_setopt($ch, CURLOPT_POST, count($paramsArray));
+      curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($paramsArray));
+      
+      //execute post
+      $result             = curl_exec($ch);
+      $this->lastResponse = ['content' => $result, 'headers' => curl_getinfo($ch)];
+      
+      //close connection
+      curl_close($ch);
+      $decodedResult = json_decode($result, $responseAsArray);
+      if ($decodedResult) {
+        $decodedResultArr = MyArray::init($decodedResult);
+        if ($countTry > 0 && $decodedResultArr->item('status', $decodedResultArr->item('success', FALSE)) == FALSE && !$this->logged) {
+          $countTry--;
+          if ($this->login($countTry)) {
+            $this->logged = TRUE;
+            $decodedResult = $this->callApi($url, $paramsArray, $responseAsArray);
+          }
         }
-    
-        return $result;
+        
+        $result = $decodedResult;
+      }
+      
+      return $result;
     }
-  
+    
     /**
      * Zajištujě zavolání API metody a data zabalí do JSON a za gzipuje.
      *
@@ -148,7 +148,7 @@
      */
     public function callApiJson($url, $paramsArray = [], $responseAsArray = TRUE) {
       $response = $this->callApi($url, [json_encode($paramsArray)], $responseAsArray);
-  
+      
       if (($data = @json_decode($response, TRUE))) {
         $response = $data;
         unset($data);
@@ -156,7 +156,7 @@
       
       return $response;
     }
-
+    
     /** @return array */
     public function getLastResponse() { return $this->lastResponse; }
     /** @param $requestTimeout */
@@ -169,19 +169,19 @@
     private function log ($url, $data) {
       // TODO: pouzit Logger, který již máme hotový
     }
-  
+    
     /**
      * @deprecated
      * @throws \Exception
      */
     public function connect() { throw new \Exception('Method ' . __METHOD__ . ' is deprecated. Now connect auto.'); }
-  
+    
     /**
      * @deprecated
      * @throws \Exception
      */
     public function isAsync() { throw new \Exception('Method ' . __METHOD__ . ' is deprecated.'); }
-  
+    
     /**
      * @deprecated
      * @throws \Exception
