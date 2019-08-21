0377dc533f06ced561a0e0d5a594acab8bb58f16
Pavel Filípek <filipek@proclient.cz>
2019-08-21 16:31:22 +0200

    -testovani composeru v2

---
+++ src/Test.php
@@ -0,0 +1,19 @@
+<?php
+  
+  namespace apiconnector;
+  
+  use PF\helpers\MyArray;
+  use PF\helpers\MyString;
+  use PF\helpers\types\JSON;
+  use Traversable;
+
+  /**
+   * Třída Response.
+   *
+   * @author    Pavel Filípek <pavel@filipek-czech.cz>
+   * @copyright © 2019, Proclient s.r.o.
+   * @created   20.02.2019
+   */
+  class Test
+  {
+  }
